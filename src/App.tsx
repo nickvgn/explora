eec93028d461d301998913bd03e3426d2e0a7a9f
nickvgn <nick.von42@gmail.com>
2025-06-01 12:23:07 +0800

    feat: unistyles + nitro modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { StyleSheet } from "react-native-unistyles";
 
 export default function App() {
 	return (
